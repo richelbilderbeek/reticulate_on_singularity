7f65b1c4c758dc9f34ffa80d331e0d39e1ec79dc
richelbilderbeek <richel@richelbilderbeek.nl>
2021-11-29 10:15:34 +0100

    Run scipy example

--- scripts/demo_container.R
+++ scripts/demo_container.R
@@ -1,2 +1,4 @@
-library(ormr)
-ormr_report(ormr_folder_name = "/opt/ormr")
+library(reticulate)
+reticulate::use_condaenv(condaenv = "/opt/ormr")
+reticulate::use_python(python = reticulate:::python_binary_path("/opt/ormr"), required = TRUE)
+reticulate::py_run_file(file = "scripts/scipy_example.py")
