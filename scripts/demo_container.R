2aa47a03b3d22f0dac46f70a00a1e527b13d90b2
richelbilderbeek <richel@richelbilderbeek.nl>
2021-11-29 10:31:16 +0100

    list files

--- scripts/demo_container.R
+++ scripts/demo_container.R
@@ -5,4 +5,6 @@ Sys.setenv(PYTHONUSERBASE = "/opt/ormr/bin/python")
 Sys.setenv(PYTHONPATH = "/opt/ormr/bin")
 reticulate::use_condaenv(condaenv = "/opt/ormr")
 reticulate::use_python(python = reticulate:::python_binary_path("/opt/ormr"), required = TRUE)
+
+list.files()
 reticulate::py_run_file(file = "scripts/scipy_example.py")
