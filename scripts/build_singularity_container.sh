cf219d11ac8dd8c0b644b498933520e3de9617dc
richelbilderbeek <richel@richelbilderbeek.nl>
2021-11-29 09:21:01 +0100

    Go GHA

---
+++ scripts/build_singularity_container.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Build the Singularity container called `ormr.sif`
+# from the Singularity recipe `Singularity` (which is a default
+# name for a Singularity recipe)
+#
+# Usage:
+#
+# ./scripts/build_singularity_container.sh
+#
+#
+
+sudo -E singularity --quiet build ormr.sif Singularity
+#sudo -E singularity --quiet build --sandbox ormr.sif Singularity
+#sudo -E singularity --quiet build --writeable ormr.sif Singularity
+
+
