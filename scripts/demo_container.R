2d70ea43f7b9199f1b0770cff937a683157c9982
richelbilderbeek <richel@richelbilderbeek.nl>
2021-11-29 10:33:24 +0100

    List files

--- scripts/demo_container.R
+++ scripts/demo_container.R
@@ -6,5 +6,14 @@ Sys.setenv(PYTHONPATH = "/opt/ormr/bin")
 reticulate::use_condaenv(condaenv = "/opt/ormr")
 reticulate::use_python(python = reticulate:::python_binary_path("/opt/ormr"), required = TRUE)
 
-list.files()
-reticulate::py_run_file(file = "scripts/scipy_example.py")
+list.files(recursive = TRUE)
+
+all_files <- list.files(recursive = TRUE, full.names = TRUE)
+
+python_filename <- stringr::str_subset(
+  string = all_files,
+  pattern = "scipy_example.py"
+)
+testthat::expect_equal(length(python_filename), 1)
+
+reticulate::py_run_file(file = python_filename)
