608705dd61a4f4c167f6f3264307257e8378f35a
richelbilderbeek <richel@richelbilderbeek.nl>
2021-11-30 12:04:50 +0100

    Do not need Python environmental variables

--- scripts/demo_container.R
+++ scripts/demo_container.R
@@ -1,9 +1,5 @@
 library(reticulate)
 
-
-# From https://github.com/rstudio/reticulate/issues/1044#issuecomment-896248800
-Sys.setenv(PYTHONUSERBASE = "/opt/ormr/bin/python")
-Sys.setenv(PYTHONPATH = "/opt/ormr/bin")
 reticulate::use_condaenv(condaenv = "/opt/ormr")
 reticulate::use_python(python = reticulate:::python_binary_path("/opt/ormr"), required = TRUE)
 
